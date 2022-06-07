cadac3ff18cd7080e2ff524f952507d6b92dde9f
Fábio R. Nóbrega <fabio.r.nobrega@gmail.com>
2022-06-07 09:27:48 -0300

    feat(setup)

---
+++ lib/postal_code/data_parser.ex
@@ -0,0 +1,29 @@
+defmodule ElhexDelivery.PostalCode.DataParser do
+  @postal_codes_filepath "data/2021_Gaz_zcta_national.txt"
+
+  def parse_data do
+    [_header | data_rows] = File.read!(@postal_codes_filepath) |> String.split("\n")
+
+    data_rows
+    |> Enum.map(fn(row) -> String.split(row, "\t") end)
+    |> Enum.filter(fn(row) ->
+      case row do
+        [_postal_code, _, _, _, _, _latitude, _longitude] -> true
+        _ -> false
+      end
+    end)
+    |> Enum.map(fn(row) ->
+      [postal_code, _, _, _, _, latitude, longitude] = row
+      [postal_code, latitude, longitude]
+      end)
+    |> Enum.map(fn(row) ->
+      [postal_code, latitude, longitude] = row
+
+      latitude = latitude |> String.replace(" ", "") |> String.to_float
+      longitude = longitude |> String.replace(" ", "") |> String.to_float
+
+      {postal_code, { latitude, longitude }}
+    end)
+    |> Enum.into(%{})
+  end
+end
