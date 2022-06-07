6c7c6e435523ab398dba8b6fe04b8b5a30c12170
Fábio R. Nóbrega <fabio.r.nobrega@gmail.com>
2022-06-07 09:48:07 -0300

    refactor(lib/postal_code/data_parser.ex): add private function to clarify code

--- lib/postal_code/data_parser.ex
+++ lib/postal_code/data_parser.ex
@@ -5,25 +5,34 @@ defmodule ElhexDelivery.PostalCode.DataParser do
     [_header | data_rows] = File.read!(@postal_codes_filepath) |> String.split("\n")
 
     data_rows
-    |> Enum.map(fn(row) -> String.split(row, "\t") end)
-    |> Enum.filter(fn(row) ->
-      case row do
-        [_postal_code, _, _, _, _, _latitude, _longitude] -> true
-        _ -> false
-      end
-    end)
-    |> Enum.map(fn(row) ->
-      [postal_code, _, _, _, _, latitude, longitude] = row
-      [postal_code, latitude, longitude]
-      end)
-    |> Enum.map(fn(row) ->
-      [postal_code, latitude, longitude] = row
-
-      latitude = latitude |> String.replace(" ", "") |> String.to_float
-      longitude = longitude |> String.replace(" ", "") |> String.to_float
-
-      {postal_code, { latitude, longitude }}
-    end)
+    |> Stream.map(&(String.split(&1, "\t")))
+    |> Stream.filter(&valid_data_row?(&1))
+    |> Stream.map(&parse_data_columns(&1))
+    |> Stream.map(&format_row(&1))
     |> Enum.into(%{})
+  end
+
+  defp valid_data_row?(row) do
+    case row do
+      [_postal_code, _, _, _, _, _latitude, _longitude] -> true
+      _ -> false
+    end
+  end
+
+  defp parse_data_columns(row) do
+    [postal_code, _, _, _, _, latitude, longitude] = row
+    [postal_code, latitude, longitude]
+  end
+
+  defp parse_number(str) do
+    str |> String.replace(" ", "") |> String.to_float
+  end
+
+  # format three element list into two element tuple
+  # [postal_code, latitude, longitude] # => { postal_code, {latitude, logitude}}
+  defp format_row([postal_code, latitude, longitude]) do
+    latitude = parse_number(latitude)
+    longitude = parse_number(longitude)
+    {postal_code, {latitude, longitude }}
   end
 end
